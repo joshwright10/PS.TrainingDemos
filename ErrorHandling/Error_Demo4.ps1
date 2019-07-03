e2d08b8912202e6e31cb4a607045af52e7a8d8cf
Josh Wright <josh.wright100@gmail.com>
2019-07-03 21:36:31 +0100

    Added Scheduled Task Error Demos

--- ErrorHandling/Error_Demo4.ps1
+++ ErrorHandling/Error_Demo4.ps1
@@ -2,47 +2,235 @@
 .DESCRIPTION
 
     Main concepts covered here:
-    -- Errors in Scripts
-    -- Exit Codes for Scripts
-
+    -- Returning the correct Exit code with Task Scheduler
 
 #>
 
 # Change Location to Current File Path.
 Set-Location C:\Git\GitHub\PS.TrainingDemos\ErrorHandling
 
+<#
+This section covers the way in which Task Scheduler handles errors from PowerShell and the Exit code that is displayed in the Run Result.
+This can be helpful in order to enable monitoring of Scheduled Tasks via 3rd party tools.
+Version 1.x tasks use the -Command parameter for PowerShell.exe and then return the $LastExitCode value.
+Version 2.x tasks use the -File parameter for PowerShell.exe and simply run the file as is.
+#>
+
+# Silently Exit with nothing
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 1.0 (User)" -ErrorAction Ignore -Confirm:$false
 $Splat_TaskAction = @{
     Execute  = "Powershell.exe"
-    Argument = "-NoProfile -ExecutionPolicy Bypass -File $(Get-Location)\Error_Demo4_Task1.ps1"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command `". $(Get-Location)\Error_Demo4_Task1.ps1 -Version 1.0; exit `$LastExitCode`""
 }
 $Splat_RegisterTask = @{
-    TaskName = "Error Handling Demo - 1"
+    TaskName = "Error Handling Demo 1.0 (User)"
     TaskPath = "\"
     Action   = New-ScheduledTaskAction @Splat_TaskAction
-}
-Register-ScheduledTask @Splat_RegisterTask
-
-Start-ScheduledTask -TaskName "Error Handling Demo - 1"
-Get-ScheduledTaskInfo -TaskName "Error Handling Demo - 1" | Select-Object TaskName, LastRunTime, LastTaskResult
-
-
-
-# Task 1.1 with $LastExitCode
-
-Unregister-ScheduledTask  -TaskName "Error Handling Demo - 1.1 - User" -ErrorAction Ignore -Confirm:$false
-$Splat_TaskAction = @{
-    Execute  = "Powershell.exe"
-    Argument = " -NoProfile -command `". $(Get-Location)\Error_Demo4_Task1.ps1; exit 3`""
-}
-$Splat_RegisterTask = @{
-    TaskName = "Error Handling Demo - 1.1 - User"
-    TaskPath = "\"
-    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
 }
 Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 1.0 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 1.0 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
 
-Start-ScheduledTask -TaskName "Error Handling Demo - 1.1 - User"
-Get-ScheduledTaskInfo -TaskName "Error Handling Demo - 1.1 - User" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+# $PSCmdlet.ThrowTerminatingError($PSItem)
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 1.1 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command `". $(Get-Location)\Error_Demo4_Task1.ps1 -Version 1.1; exit `$LastExitCode`""
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 1.1 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 1.1 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 1.1 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
 
 
-–Command "& {C:\ProgramData\ORGNAME\scripts\SetDNS.ps1; exit $LastExitCode}"
+# $PSCmdlet.WriteError($PSItem)
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 1.2 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command `". $(Get-Location)\Error_Demo4_Task1.ps1 -Version 1.2; exit `$LastExitCode`""
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 1.2 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 1.2 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 1.2 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+
+# Exit 8
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 1.3 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command `". $(Get-Location)\Error_Demo4_Task1.ps1 -Version 1.3; exit `$LastExitCode`""
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 1.3 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 1.3 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 1.3 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+
+# Write-Error -ErrorAction Stop
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 1.4 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command `". $(Get-Location)\Error_Demo4_Task1.ps1 -Version 1.4; exit `$LastExitCode`""
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 1.4 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 1.4 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 1.4 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+
+# Write-Error -ErrorAction Continue
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 1.5 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command `". $(Get-Location)\Error_Demo4_Task1.ps1 -Version 1.5; exit `$LastExitCode`""
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 1.5 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 1.5 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 1.5 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+
+#####################
+# Version 2.x Tasks #
+#####################
+
+# Silently Exit with nothing
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 2.0 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -File $(Get-Location)\Error_Demo4_Task1.ps1 -Version 2.0"
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 2.0 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 2.0 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 2.0 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+
+# $PSCmdlet.ThrowTerminatingError($PSItem)
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 2.1 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -File $(Get-Location)\Error_Demo4_Task1.ps1 -Version 2.1"
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 2.1 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 2.1 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 2.1 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+
+# $PSCmdlet.WriteError($PSItem)
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 2.2 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -File $(Get-Location)\Error_Demo4_Task1.ps1 -Version 2.2"
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 2.2 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 2.2 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 2.2 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+
+# Exit 8
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 2.3 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -File $(Get-Location)\Error_Demo4_Task1.ps1 -Version 2.3"
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 2.3 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 2.3 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 2.3 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+
+# Write-Error -ErrorAction Stop
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 2.4 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -File $(Get-Location)\Error_Demo4_Task1.ps1 -Version 2.4"
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 2.4 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 2.4 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 2.4 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
+
+
+# Write-Error -ErrorAction Continue
+Unregister-ScheduledTask  -TaskName "Error Handling Demo 2.5 (User)" -ErrorAction Ignore -Confirm:$false
+$Splat_TaskAction = @{
+    Execute  = "Powershell.exe"
+    Argument = " -NoProfile -NonInteractive -ExecutionPolicy Bypass -File $(Get-Location)\Error_Demo4_Task1.ps1 -Version 2.5"
+}
+$Splat_RegisterTask = @{
+    TaskName = "Error Handling Demo 2.5 (User)"
+    TaskPath = "\"
+    Action   = New-ScheduledTaskAction @Splat_TaskAction
+    Setting  = New-ScheduledTaskSettingsSet -Hidden
+}
+Register-ScheduledTask @Splat_RegisterTask -Force
+Start-ScheduledTask -TaskName "Error Handling Demo 2.5 (User)"
+Start-Sleep -Seconds 3
+Get-ScheduledTaskInfo -TaskName "Error Handling Demo 2.5 (User)" | Select-Object TaskName, LastRunTime, LastTaskResult
--- ErrorHandling/Error_Demo4_Task1.ps1
+++ ErrorHandling/Error_Demo4_Task1.ps1
@@ -1,2 +1,43 @@
+<#
+.SYNOPSIS
+    Basic script function to help test how Task Scheduler handles Exit codes.
 
-#[System.Environment]::Exit(1)
+.EXAMPLE
+    PS:\> Error_Demo4_Task1.ps1 -Version 1.0
+
+    Runs the section that matches Version 1.0
+#>
+
+[CmdletBinding()]
+param (
+    [double]$Version
+)
+
+if ($PSBoundParameters["Version"] -eq "1.0") {
+    # Just exit with no exit code
+}
+elseif ($PSBoundParameters["Version"] -eq "1.1") {
+    try {
+        Write-Error -Message "Version 1.1: Throwing an Error..." -ErrorAction Stop
+    }
+    catch {
+        $PSCmdlet.ThrowTerminatingError($PSItem)
+    }
+}
+elseif ($PSBoundParameters["Version"] -eq "1.2") {
+    try {
+        Write-Error -Message "Version 1.2: Throwing an Error..." -ErrorAction Stop
+    }
+    catch {
+        $PSCmdlet.WriteError($PSItem)
+    }
+}
+elseif ($PSBoundParameters["Version"] -eq "1.3") {
+    exit 8
+}
+elseif ($PSBoundParameters["Version"] -eq "1.4") {
+    Write-Error -Message "Version 1.4: Throwing an Error..." -ErrorAction Stop
+}
+elseif ($PSBoundParameters["Version"] -eq "1.5") {
+    Write-Error -Message "Version 1.5: Throwing an Error..." -ErrorAction Continue
+}
